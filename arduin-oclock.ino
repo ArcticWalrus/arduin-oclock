2ada7ce2318c02770a3c066dcc9b224e090a7daa
Malcolm St. John <malcolm19@hotmail.com>
2020-09-12 15:38:10 -0400

    removed comment

--- arduin-oclock.ino
+++ arduin-oclock.ino
@@ -45,15 +45,6 @@ void setup(){
   last_time = current_time;
 }
 
-/*   The most common method of using 74CH595 shift register
- *   latch->LOW : Begin transmitting signals.
- *   shiftOut(dataPin, clockPin, bitOrder, value)
- *   dataPin: the pin on which to output each bit. Allowed data types: int.
- *   clockPin: the pin to toggle once the dataPin has been set to the correct value. Allowed data types: int.
- *   bitOrder: which order to shift out the bits; either MSBFIRST or LSBFIRST. (Most Significant Bit First, or, Least Significant Bit First).
- *   value: the data to shift out. Allowed data types: byte. 
- *   latch->HIch : The end of the transmission signal.
-*/
 void displayTime(unsigned char num){
   digitalWrite(latch,LOW);
   shiftOut(data,clock,MSBFIRST,table[num]);
