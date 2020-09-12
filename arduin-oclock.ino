39c0bad85ea2af58ade017dd2f391735febb13d2
Malcolm St. John <malcolm19@hotmail.com>
2020-09-12 14:54:22 -0400

    Before setting time variables

--- arduin-oclock.ino
+++ arduin-oclock.ino
@@ -1,17 +1,17 @@
-// Setting pings for 
-int latch=9;  //74HC595  pin 9 STCP
-int clock=10; //74HC595  pin 10 SHCP
-int data=8;   //74HC595  pin 8 DS
+// Setting pins for shift register
+int latch = 9;  //74HC595  pin 9 STCP
+int clock = 10; //74HC595  pin 10 SHCP
+int data = 8;   //74HC595  pin 8 DS
 
-//Display digital data
-unsigned char table[]={0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71,0x00};
-unsigned char testing[] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71,0x00};
+// Array that contains the display hex for 0 through f
+unsigned char table[] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71,0x00};
 
-void setup() {
+void setup(){
   pinMode(latch,OUTPUT);
   pinMode(clock,OUTPUT);
   pinMode(data,OUTPUT);
 }
+
 /*   The most common method of using 74CH595
  *   latch->LOW : Begin transmitting signals.
  *   shiftOut(dataPin, clockPin, bitOrder, value)
@@ -21,15 +21,14 @@ void setup() {
  *   value: the data to shift out. Allowed data types: byte. 
  *   latch->HIch : The end of the transmission signal.
 */
-void displayTime(unsigned char num)
-{
+void displayTime(unsigned char num){
   digitalWrite(latch,LOW);
   shiftOut(data,clock,MSBFIRST,table[num]);
   digitalWrite(latch,HIGH);
 }
 
-void loop() {
-  for(int i = 0; i < 16; i++){
+void loop(){
+  for(int i = 0; i < 17; i++){
     displayTime(i);
     delay(500);
   }
