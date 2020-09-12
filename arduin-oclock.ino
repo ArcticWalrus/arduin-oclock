4d5b71cb7f37984948fb0e873e99b84f3b3608c9
Malcolm St. John <malcolm19@hotmail.com>
2020-09-12 15:28:36 -0400

    Pins set correctly for controlling digits

--- arduin-oclock.ino
+++ arduin-oclock.ino
@@ -3,16 +3,34 @@ int latch = 9;  //74HC595  pin 9 STCP
 int clock = 10; //74HC595  pin 10 SHCP
 int data = 8;   //74HC595  pin 8 DS
 
+// Setting pins for controlling which digit is displayed
+int minuteUnitPin = 7;
+int minuteDecPin = 6;
+int hourUnitPin = 5;
+int hourDecPin = 4;
+
 // Array that contains the display hex for 0 through f
 unsigned char table[] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71,0x00};
 
 void setup(){
+  // Pins for shift register
   pinMode(latch,OUTPUT);
   pinMode(clock,OUTPUT);
   pinMode(data,OUTPUT);
+
+  // Pins for digit control on display
+  pinMode(minuteUnitPin, OUTPUT);
+  pinMode(minuteDecPin, OUTPUT);
+  pinMode(hourUnitPin, OUTPUT);
+  pinMode(hourDecPin, OUTPUT);
+
+  digitalWrite(minuteUnitPin, HIGH);
+  digitalWrite(minuteDecPin, LOW);
+  digitalWrite(hourUnitPin, HIGH);
+  digitalWrite(hourDecPin, HIGH);
 }
 
-/*   The most common method of using 74CH595
+/*   The most common method of using 74CH595 shift register
  *   latch->LOW : Begin transmitting signals.
  *   shiftOut(dataPin, clockPin, bitOrder, value)
  *   dataPin: the pin on which to output each bit. Allowed data types: int.
